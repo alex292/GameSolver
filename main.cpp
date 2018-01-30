3c6752c7677eed8b70672cec0765a8e19d67fd9d
Alexander Hendrich <alexander.hendrich@googlemail.com>
2018-01-30 20:45:51 +0100

    create game & players from user-input

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <bitset>
 #include <cstdlib>
 #include <deque>
+#include <iostream>
 #include <vector>
 
 #include <QCoreApplication>
@@ -20,23 +21,63 @@
 #include "players/monteCarlo/montecarloplayer.h"
 #include "players/random/randomplayer.h"
 
+QString GetUserInputString(const QString &command, const QStringList &possibleValues) {
+  QString word;
+  do {
+    std::cout << command.toStdString() << " [" << possibleValues.join(", ").toStdString() << "]:";
+
+    QTextStream qtin(stdin);
+
+    qtin >> word;
+  } while (!possibleValues.contains(word));
+  return word;
+}
+
+int GetUserInputInt(const QString &command, int min, int max) {
+  int value;
+  QString word;
+  bool is_number;
+  do {
+    std::cout << command.toStdString() << " [" << min << "-" << max << "]:";
+
+    QTextStream qtin(stdin);
+    qtin >> word;
+
+    value = word.toInt(&is_number);
+
+  } while (!is_number || value < min || value > max);
+  return value;
+}
+
+std::shared_ptr<Board> CreateBoard() {
+  QString board_name = GetUserInputString("Choose board", {"tic-tac-toe", "3D-connect-four"});
+  if (board_name == "tic-tac-toe")
+    return std::make_shared<TicTacToeBoard>();
+  else
+    return std::make_shared<ConnectFour3DBoard>();
+}
+
+std::shared_ptr<Player> CreatePlayer(const QString &color) {
+  QString player_name = GetUserInputString("Choose player " + color, {"human", "random", "monte-carlo"});
+  if (player_name == "human") {
+    return std::make_shared<HumanPlayer>();
+  } else if (player_name == "random") {
+    return std::make_shared<RandomPlayer>();
+  } else {
+    int time_per_move = GetUserInputInt("time per move (ms)", 1, std::numeric_limits<int>::max());
+    int num_threads = GetUserInputInt("num threads", 1, QThread::idealThreadCount());
+    return std::make_shared<MonteCarloPlayer>(time_per_move, num_threads);
+  }
+}
+
 int main(int argc, char *argv[]) {
-  // arguments
-  int time_per_move = 10000;  // 0000000;
-  if (argc > 1)
-    time_per_move = atoi(argv[1]);
+  // std::shared_ptr<Board> board = std::make_shared<ConnectFour3DBoard>();
+  std::shared_ptr<Board> board = CreateBoard();
 
-  int num_threads = QThread::idealThreadCount();
-  if (argc > 2)
-    num_threads = atoi(argv[2]);
-
-  qDebug() << "time per move" << time_per_move;
-  qDebug() << "numThreads" << num_threads << "/" << QThread::idealThreadCount();
-
-  std::shared_ptr<Board> board = std::make_shared<ConnectFour3DBoard>();
-
-  std::shared_ptr<Player> player_white = std::make_shared<MonteCarloPlayer>(time_per_move, num_threads);
-  std::shared_ptr<Player> player_black = std::make_shared<HumanPlayer>();
+  // std::shared_ptr<Player> player_white = std::make_shared<MonteCarloPlayer>(time_per_move, num_threads);
+  std::shared_ptr<Player> player_white = CreatePlayer("white");
+  // std::shared_ptr<Player> player_black = std::make_shared<HumanPlayer>();
+  std::shared_ptr<Player> player_black = CreatePlayer("black");
 
   Game game(board, player_white, player_black);
   game.Run();
